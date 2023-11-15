fad93f9c0c2ae9bfed01de870c9e14797a690f17
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-15 21:36:56 +0100

    Remove new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :foods, only: [ :index, :new, :create, :destroy ]
-  resources :recipes, only: [ :index, :show, :new, :create, :destroy ]
+  resources :recipes, only: [ :index, :show, :destroy ]
     
   get "up" => "rails/health#show", as: :rails_health_check
 
