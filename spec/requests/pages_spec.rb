faffec42c1b67a83301f1702064ddc49ea9bdd3a
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-16 16:17:48 +0100

    Add tests for public recipe view

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /public_recipes' do
+    before do
+      get public_recipes_path
+    end
+
+    it 'returns correct status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders placeholder on page' do
+      expect(response.body).to include('Public Recipes')
+    end
   end
 end
