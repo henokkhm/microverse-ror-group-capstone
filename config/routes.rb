6263f31b114d9c192d2ad6ec24bd3b3d3e7529e3
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-17 15:08:59 +0100

    Update recipe to have new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :foods, only: [ :index, :new, :create, :destroy ]
-  resources :recipes, only: [ :index, :show, :destroy ]
+  resources :recipes, only: [ :index, :show, :new, :create, :destroy ]
   get "/public_recipes", to: "recipes#public_recipes"
 
   get 'up' => 'rails/health#show', as: :rails_health_check
