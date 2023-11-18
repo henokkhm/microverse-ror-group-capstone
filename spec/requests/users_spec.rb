9c3de232c95d2a485ec62737424c81dc2decce7b
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-18 16:21:03 +0100

    Fix users controller test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET request to endpoint "/users/:id"' do
     before(:each) do
-      user = User.create(name: 'Henok')
-      get user_path(user.id)
+      user = User.create(name: 'tom', email: 'name@gmail.com', password: '12345678')
+      sign_in user
+      get user_path(user)
     end
 
     it 'return successful response' do
