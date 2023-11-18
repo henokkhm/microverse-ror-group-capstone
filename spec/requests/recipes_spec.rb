1ee9856ac04f1b64c8d4a514c3858aa755b3d281
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-18 14:06:28 +0100

    Move pages_spec to recipes_spec

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -45,4 +45,18 @@ RSpec.describe 'Recipes', type: :request do
       expect(flash[:notice]).to eq('Recipe deleted successfully!')
     end
   end
+  
+  describe 'GET /public_recipes' do
+    before do
+      get public_recipes_path
+    end
+
+    it 'returns correct status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders placeholder on page' do
+      expect(response.body).to include('Public Recipes')
+    end
+  end
 end
